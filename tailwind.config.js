7920f07e28800f321515e8a4af6a29e9e4ec9f27
champsing <88385811+champsing@users.noreply.github.com>
2024-11-25 10:09:59 +0800

    establish the basic structure  of the package

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./web/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            height: {
+                '80vh': '80vh',
+                '40vh': '40vh'
+            }
+        },
+    },
+    plugins: [],
+};
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler",
+            },
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.indexOf("node_modules") != -1) {
+                        return id
+                            .toString()
+                            .split("node_modules/")[1]
+                            .split("/")[0]
+                            .toString();
+                    }
+                },
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+            "@assets": fileURLToPath(new URL("./src/assets", import.meta.url)),
+            "@components": fileURLToPath(
+                new URL("./src/components", import.meta.url)
+            ),
+            "@composables": fileURLToPath(
+                new URL("./src/composables", import.meta.url)
+            ),
+        },
+    },
+});
